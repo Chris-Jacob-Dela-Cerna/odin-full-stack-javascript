a20fa4c04cf99b8481aeda07720646011862e684
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-09-19 20:44:00 +0800

    fix: add missing semi-colons

--- intermediate-javascript/script.js
+++ intermediate-javascript/script.js
@@ -7,9 +7,17 @@ function Book(title=null, author=undefined, pages=0, read=false) {
   this.title = title,
   this.author = author,
   this.pages = pages,
-  this.read = read
+  this.read = read,
+  this.info = function () {
+    let isRead = null;
+    if (this.read) isRead = "has been read";
+    else isRead = "not yet read";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead}.`;
+  }
 }
 
 const bookNull = new Book();
 // The Hobbit by J.R.R. Tolkien, 295 pages, not read yet
 const bookTheHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+console.log(bookTheHobbit.info());
